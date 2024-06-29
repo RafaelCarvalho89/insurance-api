44987d07174ca6fab4945f4b1e95b9c7149842ae
Rafael Carvalho <carvalho.rafaelfp@gmail.com>
2024-06-28 23:08:22 -0300

    feat: create InsuranceRules IncomeMethods

---
+++ app/core/insurance_rules/methods/income.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# InsuranceRules module
+module InsuranceRules
+  # IncomeMethods module
+  HIGHEST_INCOME = 200_000
+
+  # IncomeMethods module
+  module IncomeMethods
+    def validate_income_input(income)
+      raise ArgumentError, 'Income must be an integer' unless income.is_a?(Integer)
+      raise ArgumentError, 'Income must be a non-negative integer' unless income >= 0
+    end
+
+    def income?(income)
+      validate_income_input(income)
+      income.positive?
+    rescue ArgumentError => e
+      raise ArgumentError, e.message
+    end
+
+    def income_more_than_200_000?(income)
+      validate_income_input(income)
+      income > HIGHEST_INCOME
+    rescue ArgumentError => e
+      raise ArgumentError, e.message
+    end
+  end
+end
